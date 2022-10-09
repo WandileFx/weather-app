d334b9798c8a9c9101c2f57ce2a89ed418e95e87
WandileFx <112031600+WandileFx@users.noreply.github.com>
2022-10-10 00:16:41 +0200

    Add files via upload

---
+++ weather-app/script.js
@@ -0,0 +1,47 @@
+let weather = {
+    "apiKey": "585c62d2de888632f5e28577a5c2c27a",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKey
+        )
+            .then((res) => res.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+
+
+
+        document.querySelector(".city").innerHTML = "Weather in " + name;
+
+        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".temp").innerHTML = temp + "°C";
+        document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerHTML = "Wind speed: " + Math.floor(speed * 3.6)
+            + " Km/h";
+        document.querySelector(".weather").classList.remove("loading");
+
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+weather.fetchWeather("pretoria");
